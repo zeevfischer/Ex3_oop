3aec60c2d5833a4eed12b752935b3e04441bd1c2
liavm1998 <liavmoshe12345@gmail.com>
2021-12-27 15:18:40 +0200

    122

--- classes/GUI.py
+++ classes/GUI.py
@@ -12,20 +12,24 @@ pygame.font.init()
 clock = pygame.time.Clock()
 FONT = pygame.font.SysFont('comicsans', 20)
 
+
 def scale(data: int, min_screen, max_screen, min_data, max_data):
     """
     get the scaled data with proportions min_data, max_data
     relative to min and max screen dimensions
     """
-    return ((float(data) - float(min_data)) / (float(max_data)-float(min_data))) * (float(max_screen) - float(min_screen)) + float(min_screen)
+    return ((float(data) - float(min_data)) / (float(max_data) - float(min_data))) * (
+                float(max_screen) - float(min_screen)) + float(min_screen)
+
 
 min_x = sys.float_info.max
 min_y = sys.float_info.max
 max_x = 0
 max_y = 0
-def min_max(graph):
 
-    global min_x,min_y,max_x,max_y
+
+def min_max(graph):
+    global min_x, min_y, max_x, max_y
     for node in graph.Nodes.values():
         if float(node.pos.x) < float(min_x):
             min_x = node.pos.x
@@ -39,21 +43,24 @@ def min_max(graph):
         if float(node.pos.y) > float(max_y):
             max_y = node.pos.y
 
+
 class Button:
-    def __init__(self, rect:pygame.Rect,color:tuple=(0,0,0)):
-        self.rect=rect
-        self.color=color
-        self.pressed=False
+    def __init__(self, rect: pygame.Rect, color: tuple = (0, 0, 0)):
+        self.rect = rect
+        self.color = color
+        self.pressed = False
+
     def press(self):
-        self.pressed=not self.pressed
+        self.pressed = not self.pressed
+
 
 class Gui:
     pygame.font.init()
     clock = pygame.time.Clock()
     FONT = pygame.font.SysFont('comicsans', 20)
 
-    def __init__(self,algo=None):
-        self.algo=algo
+    def __init__(self, algo=None):
+        self.algo = algo
         self.screen = pygame.display.set_mode((800, 600), depth=32, flags=RESIZABLE)
         min_max(algo.graph)
         self.button1 = Button(pygame.Rect(0, 0, 100, 50), (200, 200, 200))
@@ -64,10 +71,10 @@ class Gui:
         self.button6 = Button(pygame.Rect(545, 0, 100, 50), (200, 200, 200))
         self.button7 = Button(pygame.Rect(650, 0, 70, 50), (200, 200, 200))
 
-        self.list_of_algo=[]
+        self.list_of_algo = []
         self.play()
 
-    def arrow(self,start, end, d, h, color):
+    def arrow(self, start, end, d, h, color):
         dx = (end[0] - start[0])
         dy = (end[1] - start[1])
         D = (math.sqrt(dx * dx + dy * dy))
@@ -88,7 +95,7 @@ class Gui:
         pygame.draw.line(self.screen, color, start, end, width=4)
         pygame.draw.polygon(self.screen, color, points)
 
-    def my_scale(self,data, x=False, y=False):
+    def my_scale(self, data, x=False, y=False):
         if x:
             return scale(data, 100, self.screen.get_width() - 100, min_x, max_x)
         if y:
@@ -102,7 +109,7 @@ class Gui:
         for num in range(11):
             src_x = 0 + (num * width)
             src_y = 0
-            dest_x = 0+(num*width)
+            dest_x = 0 + (num * width)
             dest_y = self.my_scale(hight, y=True)
             Gui.arrow(self, (src_x, src_y), (dest_x, dest_y), 0, 0, color=(148, 148, 148))
 
@@ -113,9 +120,9 @@ class Gui:
         # ---- lines
         for num in range(11):
             src_x = 0
-            src_y = 0 +(num* hight)
-            dest_x = self.my_scale(width,x =True)
-            dest_y = 0+ (num*hight)
+            src_y = 0 + (num * hight)
+            dest_x = self.my_scale(width, x=True)
+            dest_y = 0 + (num * hight)
             Gui.arrow(self, (src_x, src_y), (dest_x, dest_y), 0, 0, color=(148, 148, 148))
 
             # text = str(num)
@@ -132,7 +139,7 @@ class Gui:
             dest_x = self.my_scale(self.algo.graph.Nodes.get(dest).pos.x, x=True)
             dest_y = self.my_scale(self.algo.graph.Nodes.get(dest).pos.y, y=True)
 
-            Gui.arrow(self, (src_x, src_y), (dest_x, dest_y), 30, 8, color=(63,97, 235))
+            Gui.arrow(self, (src_x, src_y), (dest_x, dest_y), 30, 8, color=(63, 97, 235))
 
         for src in self.algo.graph.Nodes.values():
             x = self.my_scale(src.pos.x, x=True)
@@ -142,7 +149,7 @@ class Gui:
             node_text = FONT.render(str(src.id), True, (255, 255, 255))
             self.screen.blit(node_text, (x - 6, y - 13))
 
-        #button activation here i set all the buttons on the graph
+        # button activation here i set all the buttons on the graph
         # here you can also see what each button dose acording to its string
         pygame.draw.rect(self.screen, self.button1.color, self.button1.rect)
         button_text = FONT.render("add node", True, (0, 0, 0))
@@ -172,10 +179,10 @@ class Gui:
         button_text = FONT.render("TSP", True, (0, 0, 0))
         self.screen.blit(button_text, (self.button7.rect.x + 10, self.button7.rect.y + 5))
 
-    def on_click(self,event):
+    def on_click(self, event):
         if event == "button1":
-            id,pos = GUI_functions.add_node()
-            self.algo.graph.add_node(id,pos)
+            id, pos = GUI_functions.add_node()
+            self.algo.graph.add_node(id, pos)
             Gui.play(self)
 
         if event == "button2":
@@ -184,24 +191,24 @@ class Gui:
             Gui.play(self)
 
         if event == "button3":
-            src,dest = GUI_functions.shortest_path()
-            sum , path = self.algo.shortest_path(src,dest)
+            src, dest = GUI_functions.shortest_path()
+            sum, path = self.algo.shortest_path(src, dest)
             GUI_functions.print("the path is " + str(path), "shortest path ")
             Gui.play(self)
 
         if event == "button4":
-            id , sum = self.algo.centerPoint()
+            id, sum = self.algo.centerPoint()
             GUI_functions.print("the center Node is " + str(id), "center")
             Gui.play(self)
 
         if event == "button5":
-            src , dest,weight = GUI_functions.add_Edge()
-            self.algo.graph.add_edge(src,dest,weight)
+            src, dest, weight = GUI_functions.add_Edge()
+            self.algo.graph.add_edge(src, dest, weight)
             Gui.play(self)
 
         if event == "button6":
-            src , dest = GUI_functions.remove_Edge()
-            self.algo.graph.remove_edge(src,dest)
+            src, dest = GUI_functions.remove_Edge()
+            self.algo.graph.remove_edge(src, dest)
             Gui.play(self)
 
         if event == "button7":
@@ -215,13 +222,13 @@ class Gui:
 
     def play(self):
         min_max(self.algo.graph)
-        run=True
+        run = True
         while run:
             clock.tick(60)
             for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    run=False
-                if event.type==pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.QUIT:
+                    run = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
                     # button one action
                     if self.button1.rect.collidepoint(event.pos):
                         self.button1.press()
@@ -281,6 +288,3 @@ class Gui:
 #     a=GraphAlgo()
 #     a.load_from_json("../data/A1.json")
 #     Gui(a)
-
-
-
